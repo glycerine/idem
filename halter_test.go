55390436990a39d951238e92a14d4454abfbaca9
Jason E. Aten, Ph.D. <jason@devnull>
2025-01-16 13:17:25 +0000

    atg. add child add and remove, another test

--- halter_test.go
+++ halter_test.go
@@ -82,7 +82,9 @@ func Test103ChildClose(t *testing.T) {
 
 		parent := NewIdemCloseChan()
 		child := NewIdemCloseChan()
+		child2 := NewIdemCloseChan()
 		parent.AddChild(child)
+		parent.AddChild(child2)
 
 		parent.Close()
 		if child.IsClosed() {
@@ -90,20 +92,66 @@ func Test103ChildClose(t *testing.T) {
 		} else {
 			panic("child should have been closed!")
 		}
+
+		if child2.IsClosed() {
+			cv.So(true, cv.ShouldEqual, true)
+		} else {
+			panic("child2 should have been closed!")
+		}
+
 	})
 
 	cv.Convey("after IdemCloseChan RemoveChild, the should not be closed when parent is", t, func() {
 		parent := NewIdemCloseChan()
 		child := NewIdemCloseChan()
+		child2 := NewIdemCloseChan()
 		parent.AddChild(child)
+		parent.AddChild(child2)
 		parent.RemoveChild(child)
+		parent.RemoveChild(child2)
 
 		parent.Close()
+
 		if child.IsClosed() {
 			panic("child should NOT have been closed!")
 		} else {
 			// good child was not closed.
 			cv.So(true, cv.ShouldEqual, true)
 		}
+
+		if child2.IsClosed() {
+			panic("child2 should NOT have been closed!")
+		} else {
+			// good child2 was not closed.
+			cv.So(true, cv.ShouldEqual, true)
+		}
 	})
+
+	cv.Convey("after IdemCloseChan RemoveChild, the should not be closed, but the rest should be,  when parent is closed", t, func() {
+		parent := NewIdemCloseChan()
+		child := NewIdemCloseChan()
+		child2 := NewIdemCloseChan()
+		parent.AddChild(child)
+		parent.AddChild(child2)
+		parent.RemoveChild(child)
+		//skip: parent.RemoveChild(child2)
+
+		parent.Close()
+
+		if child.IsClosed() {
+			panic("child should NOT have been closed!")
+		} else {
+			// good child was not closed.
+			cv.So(true, cv.ShouldEqual, true)
+		}
+
+		// child2 is still on the parent's list
+		if child2.IsClosed() {
+			// good child2 was closed.
+			cv.So(true, cv.ShouldEqual, true)
+		} else {
+			panic("child2 should have been closed!")
+		}
+	})
+
 }
